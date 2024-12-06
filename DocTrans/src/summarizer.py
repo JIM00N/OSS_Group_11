51076ffa11a68e0c705c9d13f414a26bf42abf8b
shin269 <shinho269@naver.com>
2024-12-07 00:35:39 +0900

    delete files

--- DocTrans/src/summarizer.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-def summarize(text):
-    summarizer = pipeline("summarization")
-    summary = summarizer(text, max_length=50, min_length=25, do_sample=False)
-    return summary[0]['summary_text']
